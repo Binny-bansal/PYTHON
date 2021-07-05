533df3c5cb1addd831cd474c20e3fafff4559eb6
Binny-bansal <84627527+Binny-bansal@users.noreply.github.com>
2021-07-05 15:37:13 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,62 @@
+# print("Binny!", end=" ")
+# print("Riya")
+# # num = int(input("Enter a number: ")) 
+# # print(num)
+# # print(2**5)
+# money = 100
+# item = 90
+# chocolate = 20
+# if(money-item >= chocolate):
+#         print("Yes! We can buy chocolate.")
+
+# elif(money-item < 20):
+#         print("You're low on money!")
+
+# n = int(input("Enter a Number: "))
+
+# if (n%2==0):
+#  if (n>2 and n<5):
+#     print("Not Weird")
+ 
+#  if(n>6 and n<20):
+#     print("Weird")
+
+#  if (n>20):
+#     print("Not Weird")
+
+# else:
+#  print("Not Success!")
+
+# n = int(input("Enter a Number: "))
+
+# for i in range(n):
+#         print(i**2)
+
+# sum = 0
+# n = int(input("Enter a Number: "))
+# i=0
+# while i<=n:
+#         sum+=i
+#         i+=1
+# print(sum)
+
+# n=7
+# print(n**3)
+# i=1
+# while i<10:
+#         if i==5:
+#                 continue
+#         print(i)10
+#         i+=1
+
+n = int(input("Enter any Number: "))
+sum=0
+for i in range(1,n+1):
+        
+       
+                sum+=i*i
+                
+              
+
+print(sum)               
+                
